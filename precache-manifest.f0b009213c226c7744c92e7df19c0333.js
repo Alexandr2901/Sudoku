a57ad56826912e5e05c735a73be541ba57a90586
unknown <zitov01@mail.ru>
2020-10-31 18:10:11 +0300

    deploy

---
+++ precache-manifest.f0b009213c226c7744c92e7df19c0333.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd62358540750c9c636d",
+    "url": "css/chunk-1e661698.2305756a.css"
+  },
+  {
+    "revision": "0d0725de46a48ca48184",
+    "url": "css/chunk-2a972d7c.8caada34.css"
+  },
+  {
+    "revision": "46c6769801497aae35f86bdac9182ba2",
+    "url": "index.html"
+  },
+  {
+    "revision": "d10e47d39fc6420f977f",
+    "url": "js/app.55698a3a.js"
+  },
+  {
+    "revision": "dd62358540750c9c636d",
+    "url": "js/chunk-1e661698.ae015d79.js"
+  },
+  {
+    "revision": "0d0725de46a48ca48184",
+    "url": "js/chunk-2a972d7c.47a07a54.js"
+  },
+  {
+    "revision": "a9c84603c4f3f7221e9d",
+    "url": "js/chunk-vendors.90dc7247.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
