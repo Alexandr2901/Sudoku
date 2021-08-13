1049a54b661e6a904f122ade4ebbc1a29c7ba9e7
oleg <zitov01@mail.ru>
2021-08-13 22:42:59 +0300

    travis auto build 7

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -30,7 +30,6 @@
 <!--        <a href="http://0.0.0.0/api/login/vkontakte">-->
           войти через вк
 <!--        </a>-->
-
       </button>
       <button @click="signInSocial('github')">
         <!--        <a href="http://0.0.0.0/api/login/vkontakte">-->
