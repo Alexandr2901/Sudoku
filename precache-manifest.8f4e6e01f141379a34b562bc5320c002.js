2d0bba193f8aad3391f0410e4b6832efcd68f0fd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-14 18:45:33 +0000

    Deploy Alexandr2901/Sudoku to github.com/Alexandr2901/Sudoku.git:gh-pages

---
+++ precache-manifest.8f4e6e01f141379a34b562bc5320c002.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8927e6300c725d646b",
+    "url": "/Sudoku/css/app.17dc6af5.css"
+  },
+  {
+    "revision": "e420fdd8cb5d7da00c5c",
+    "url": "/Sudoku/css/chunk-19a19c2c.cae811ee.css"
+  },
+  {
+    "revision": "08349f3f40afce29344b",
+    "url": "/Sudoku/css/chunk-23ca93f1.48084ff2.css"
+  },
+  {
+    "revision": "30996af57ae6d024433c",
+    "url": "/Sudoku/css/chunk-2a8b24d2.3d5d1b26.css"
+  },
+  {
+    "revision": "f7cea32dc353580df1a7",
+    "url": "/Sudoku/css/chunk-44002a52.b6541d7e.css"
+  },
+  {
+    "revision": "0c4d13913f347370d279",
+    "url": "/Sudoku/css/chunk-55df9ff3.834eb4e3.css"
+  },
+  {
+    "revision": "69ddd139bc5ff75c1ea6dc06624a7762",
+    "url": "/Sudoku/index.html"
+  },
+  {
+    "revision": "7f8927e6300c725d646b",
+    "url": "/Sudoku/js/app.07a67c05.js"
+  },
+  {
+    "revision": "e420fdd8cb5d7da00c5c",
+    "url": "/Sudoku/js/chunk-19a19c2c.896ae298.js"
+  },
+  {
+    "revision": "08349f3f40afce29344b",
+    "url": "/Sudoku/js/chunk-23ca93f1.7aafa10b.js"
+  },
+  {
+    "revision": "30996af57ae6d024433c",
+    "url": "/Sudoku/js/chunk-2a8b24d2.782498ea.js"
+  },
+  {
+    "revision": "f7cea32dc353580df1a7",
+    "url": "/Sudoku/js/chunk-44002a52.a2ec2709.js"
+  },
+  {
+    "revision": "0c4d13913f347370d279",
+    "url": "/Sudoku/js/chunk-55df9ff3.42114c74.js"
+  },
+  {
+    "revision": "b97de0af7dcf332cce38",
+    "url": "/Sudoku/js/chunk-vendors.4bd554dc.js"
+  },
+  {
+    "revision": "1ecee0c343b1bc0443f2e6eba14ed6ff",
+    "url": "/Sudoku/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Sudoku/robots.txt"
+  }
+]);
